5cf909e5b1c6f99773054e1bad606fb875dd8032
Gineth <ginysp@hotmail.com>
2021-03-19 03:25:10 +0000

    .

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,6 +3,28 @@ import React, { useState, useEffect } from "react";
 export function Home() {
 	let [lista, setLista] = useState([]);
 
+	fetch("https://assets.breatheco.de/apis/fake/todos/user/gsalazarp", {
+		method: "POST",
+		body: JSON.stringify(lista),
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+		.then(resp => {
+			console.log(resp.ok); // will be true if the response is successfull
+			console.log(resp.status); // the status code = 200 or code = 400 etc.
+			console.log(resp.text()); // will try return the exact result as string
+			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+		})
+		.then(data => {
+			//here is were your code should start after the fetch finishes
+			console.log(data); //this will print on the console the exact object received from the server
+		})
+		.catch(error => {
+			//error handling
+			console.log(error);
+		});
+
 	const addTarea = tarea => {
 		if (tarea.key === "Enter") {
 			setLista([...lista, tarea.target.value]);
@@ -46,6 +68,9 @@ export function Home() {
 				</div>
 			</div>
 			<p>Total Items: {lista.length}</p>
+			<button className="btn btn-info">
+				<i className="fa fa-trash"></i> Clear List
+			</button>
 		</div>
 	);
 }
